b1f9d6f1b15592f39b07d42e36ad93e867b12f9d
Johan Laanstra <jlaanstra1221@outlook.com>
2015-05-29 11:44:01 +0200

    Map the models.

---
+++ ReactiveUIXamarin-Core/Models/TinEyeResponses.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Coolness: You don't have to actually write this code at all, 
+// just go to json2csharp.com or download JsonCSharpClassGenerator 
+// from Codeplex. The latter has more flexibility and configuration 
+// options.
+namespace ReactiveUIXamarin.Core.Models
+{
+    public class Metadata
+    {
+        public string photoID { get; set; }
+        public string userID { get; set; }
+        public string imageHeight { get; set; }
+        public string imageWidth { get; set; }
+    }
+
+    public class Result
+    {
+        public string score { get; set; }
+        public Metadata metadata { get; set; }
+        public string filepath { get; set; }
+    }
+
+    public class Results
+    {
+        public string count { get; set; }
+        public string status { get; set; }
+        public List<string> error { get; set; }
+        public string method { get; set; }
+        public List<Result> result { get; set; }
+    }
+}
--- ReactiveUIXamarin-Core/ViewModels/AppBootstrapper.cs
+++ ReactiveUIXamarin-Core/ViewModels/AppBootstrapper.cs
@@ -5,6 +5,7 @@ using Splat;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
